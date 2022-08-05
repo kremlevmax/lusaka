fed580dd45f507202b8fdb4104855745aac2ffea
kremlevmax <kremlevmax1989@gmail.com>
2022-08-04 19:12:59 -0700

    Done

--- pages/donate.js
+++ pages/donate.js
@@ -1,5 +1,29 @@
+import Image from "next/image";
+import image03 from "../images/03.jpeg";
+import styles from "../styles/modules/Donate.module.scss";
+
 const donate = () => {
-  return <div>donate</div>;
+  return (
+    <section className={styles.donate}>
+      <div className={styles.donate__container}>
+        <div className={styles.donate__text}>
+          <h2>Make a Difference</h2>
+          <p>
+            Use this page to describe your company&apos;s mission and goals.
+            Nullam tempor dolor sed nulla auctor, nec placerat felis sodales.
+            Etiam et turpis mattis, efficitur mi ut, ultrices diam. Donec
+            consectetur, odio eget porta varius, orci mauris viverra ante, eget
+            egestas turpis sapien vel orci. Donec eu ornare augue, ut efficitur
+            velit. Vestibulum et magna mattis, sollicitudin ligula ac, facilisis
+            dui. Ut blandit lectus neque.
+          </p>
+        </div>
+        <div className={styles.donate__img}>
+          <Image src={image03} alt='Materials for Learning' />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default donate;
--- pages/ourteam.js
+++ pages/ourteam.js
@@ -1,13 +1,60 @@
-import Slider from "../components/Slider";
 import styles from "../styles/modules/Ourteam.module.scss";
 
 const ourteam = () => {
   return (
-    <section className={styles.ourteam}>
+    <div className={styles.ourteam}>
       <div className={styles.ourteam__container}>
-        <Slider />
+        <div className={styles.ourteam__post}>
+          <div
+            className={styles.ourteam__post_img}
+            id={styles.image01}
+            alt='Materials for Learning'
+          />
+
+          <div className={styles.ourteam__post_desc}>
+            <p className={styles.ourteam__post_desc_date}>
+              First Name Last Name
+            </p>
+          </div>
+        </div>
+        <div className={styles.ourteam__post}>
+          <div
+            className={styles.ourteam__post_img}
+            id={styles.image02}
+            alt='Materials for Learning'
+          />
+          <div className={styles.ourteam__post_desc}>
+            <p className={styles.ourteam__post_desc_date}>
+              First Name Last Name
+            </p>
+          </div>
+        </div>
+        <div className={styles.ourteam__post}>
+          <div
+            className={styles.ourteam__post_img}
+            id={styles.image03}
+            alt='Materials for Learning'
+          />
+          <div className={styles.ourteam__post_desc}>
+            <p className={styles.ourteam__post_desc_date}>
+              First Name Last Name
+            </p>
+          </div>
+        </div>
+        <div className={styles.ourteam__post}>
+          <div
+            className={styles.ourteam__post_img}
+            id={styles.image04}
+            alt='Materials for Learning'
+          />
+          <div className={styles.ourteam__post_desc}>
+            <p className={styles.ourteam__post_desc_date}>
+              First Name Last Name
+            </p>
+          </div>
+        </div>
       </div>
-    </section>
+    </div>
   );
 };
 
